8605c6960bd057940b515212fdbad386c6c99552
Filip Stanek <stanek.filip@icloud.com>
2016-05-06 16:26:01 -0500

    minor changes

--- session.js
+++ session.js
@@ -1,36 +1,39 @@
 "use strict";
-
 var encryption = require('./encryption');
 var formidable = require('formidable');
 var view = require('./view');
 var Farmer = require('./models/app_model').farmer;
 
+const error_message = "Username/Password not found. Please try again.";
+
 class Session {
 
+  // Renders a Login page
   new(req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
     res.end(view.partial('session/new', {message: "", user: req.farmer}));
   }
 
-  create(req, res, next) {
+  // Create session and makes user logged in
+  create(req, res) {
     req.session.reset();
     var form = new formidable.IncomingForm();
-    form.parse(req, (err, fields, files) => {
+    form.parse(req, (err, fields) => {
       if(err) return res.sendStatus(500);
       Farmer.where("email ='" + fields.email + "'", function (err, farmers) {
         if(err) {
           return res.end(view.partial('session/new', {
-            message: "Username/Password not found.  Please try again.",
+            message: error_message,
             farmer: req.farmer
           }));
         }
         var farmer = farmers[0];
         if(!farmer){
-          return res.end(view.partial('session/new', {message: "Username/Password not found.  Please try again.", user: req.farmer}));
+          return res.end(view.partial('session/new', {message: error_message, user: req.farmer}));
         }
         if(farmer.password != encryption.digest(fields.password + farmer.salt)) {
           return res.end(view.partial('session/new', {
-            message: "Username/Password not found.  Please try again.",
+            message: error_message,
             farmer: req.farmer
           }));
         }
