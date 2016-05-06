c3066b4e9831730ec6ed7cf41d85b1784ebce12d
Filip Stanek <stanek.filip@icloud.com>
2016-05-06 16:10:11 -0500

    logo and error message added to login

--- client/login.js
+++ client/login.js
@@ -18,6 +18,10 @@ module.exports = exports = function() {
         elem.append($ripple);
     };
 
+    if ($('.login_error').is(':empty')){
+        $('.login_error').hide();
+    }
+
     $(document).on("click", ".login__submit", function(e) {
         if (animating) return;
         animating = true;
